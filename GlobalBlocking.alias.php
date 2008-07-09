9e600654e0a236df629bb3a0c64b4d3612d63145
Jon Harald Søby <jhsoby@users.mediawiki.org>
2008-07-09 18:42:25 +0000

    ... and adding the alias files

---
+++ GlobalBlocking.alias.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Aliases for Special:GlobalBlocking
+ *
+ * @addtogroup Extensions
+ */
+
+$aliases = array();
+
+/** English
+ * @author Jon Harald Søby
+ */
+$aliases['en'] = array(
+    'GlobalBlocking'            => array('GlobalBlocking')
+);
+
+/** Norwegian (Norsk (bokmål))
+ * @author Jon Harald Søby
+ */
+$aliases['no'] = array(
+    'GlobalBlocking'            => array('Blokker_globalt', 'Global_blokkering')
+);
