3a4f483709697e794cf207b5906b1ea4027c0fa9
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-07-13 13:41:02 +0000

    Localisation updates for extension special pages from Betawiki

--- GlobalBlocking.alias.php
+++ GlobalBlocking.alias.php
@@ -14,11 +14,46 @@ $aliases['en'] = array(
     'GlobalBlocking' => array( 'GlobalBlocking' ),
 );
 
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'GlobalBlocking' => array( 'منع عام' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'GlobalBlocking' => array( 'منع عام' ),
+);
+
+/** French (Français) */
+$aliases['fr'] = array(
+	'GlobalBlocking' => array( 'BlocageGlobal' ),
+);
+
 /** Hebrew (עברית)
  * @author Rotem Liss
  */
 $aliases['he'] = array(
     'GlobalBlocking' => array( 'חסימה_כללית' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$aliases['ht'] = array(
+	'GlobalBlocking' => array( 'BlokajGlobal' ),
+);
+
+/** Hungarian (Magyar) */
+$aliases['hu'] = array(
+	'GlobalBlocking' => array( 'Globális blokkolás' ),
+);
+
+/** Malay (Bahasa Melayu) */
+$aliases['ms'] = array(
+	'GlobalBlocking' => array( 'Sekatan_sejagat' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'GlobalBlocking' => array( 'GlobaalBlokkeren' ),
 );
 
 /** Norwegian (Norsk (bokmål))
@@ -27,3 +62,4 @@ $aliases['he'] = array(
 $aliases['no'] = array(
     'GlobalBlocking' => array( 'Blokker_globalt', 'Global_blokkering' ),
 );
+
