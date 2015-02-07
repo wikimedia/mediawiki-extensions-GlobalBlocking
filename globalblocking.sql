21add05bb2ebc3fb5247dde844c8254bbcd30e34
paladox <thomasmulhall410@yahoo.com>
2015-02-07 21:46:41 +0100

    Add support for sqlite
    * Missing sqlite support in localdb_patches folder.
    
    Bug: T69300
    Change-Id: I3c697e31e486dca43a0733a45aab6d72861801fc

--- globalblocking.sql
+++ globalblocking.sql
@@ -1,4 +1,4 @@
-CREATE TABLE /*$wgDBprefix*/globalblocks (
+CREATE TABLE /*_*/globalblocks (
 	gb_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	gb_address varchar(255) NOT NULL,
 	gb_by varchar(255) NOT NULL,
@@ -11,12 +11,12 @@ CREATE TABLE /*$wgDBprefix*/globalblocks (
 	gb_range_end varbinary(35) NOT NULL
 ) /*$wgDBTableOptions*/;
 
-CREATE UNIQUE INDEX gb_address ON /*$wgDBprefix*/globalblocks (gb_address, gb_anon_only);
-CREATE INDEX gb_range ON /*$wgDBprefix*/globalblocks (gb_range_start, gb_range_end);
-CREATE INDEX gb_timestamp ON /*$wgDBprefix*/globalblocks (gb_timestamp);
-CREATE INDEX gb_expiry ON /*$wgDBprefix*/globalblocks (gb_expiry);
+CREATE UNIQUE INDEX /*i*/gb_address ON /*_*/globalblocks (gb_address, gb_anon_only);
+CREATE INDEX /*i*/gb_range ON /*_*/globalblocks (gb_range_start, gb_range_end);
+CREATE INDEX /*i*/gb_timestamp ON /*_*/globalblocks (gb_timestamp);
+CREATE INDEX /*i*/gb_expiry ON /*_*/globalblocks (gb_expiry);
 
-CREATE TABLE /*$wgDBprefix*/global_block_whitelist (
+CREATE TABLE /*_*/global_block_whitelist (
 	gbw_id int(11) NOT NULL PRIMARY KEY, -- Key to gb_id in globalblocks database.
 	gbw_address varbinary(255) NOT NULL,
 	gbw_by int(11) NOT NULL, -- Key to user_id
@@ -24,4 +24,5 @@ CREATE TABLE /*$wgDBprefix*/global_block_whitelist (
 	gbw_reason varchar(255) NOT NULL,
 	gbw_expiry binary(14) NOT NULL
 ) /*$wgDBTableOptions*/;
-CREATE INDEX gbw_by ON /*$wgDBprefix*/global_block_whitelist (gbw_by);
+
+CREATE INDEX /*i*/gbw_by ON  /*_*/global_block_whitelist (gbw_by);
