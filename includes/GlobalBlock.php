4494e49e2c234bc700c38a26dfa49dfa61998cdf
Kunal Mehta <legoktm@member.fsf.org>
2017-12-12 12:12:13 -0800

    Update for Block::setBlocker() requiring a valid user or interwiki username
    In prepraration for the upcoming actor table, Block requires the blockers
    to either be a local user or an interwiki username.
    
    We can use CentralIdLookup to check whether the blocker on the foreign wiki
    is the same as the local wiki, otherwise fallback to an interwiki username
    that uses the wiki as a prefix.
    
    Bug: T182344
    Change-Id: Ia3c85fced974a4edabc172bd3ef6a7a255ffa462

--- includes/GlobalBlock.php
+++ includes/GlobalBlock.php
@@ -14,7 +14,7 @@ class GlobalBlock extends Block {
 		parent::__construct();
 
 		$this->error = $error;
-		$this->setBlocker( $block->gb_by );
+		$this->setGlobalBlocker( $block );
 	}
 
 	/**
@@ -22,5 +22,32 @@ class GlobalBlock extends Block {
 	 */
 	public function getPermissionsError( IContextSource $context ) {
 		return $this->error;
+	}
+
+	/**
+	 * Block requires that the blocker exist or be an interwiki username, so do
+	 * some validation to figure out what we need to use (T182344)
+	 *
+	 * @param stdClass $block DB row from globalblocks table
+	 */
+	public function setGlobalBlocker( stdClass $block ) {
+		// If the block was inserted from this wiki, then we know the blocker exists
+		if ( $block->gb_by_wiki === wfWikiID() ) {
+			$this->setBlocker( $block->gb_by );
+			return;
+		}
+		$user = User::newFromName( $block->gb_by );
+		// If the blocker is the same user on the foreign wiki and the current wiki
+		// then we can use the username
+		$lookup = CentralIdLookup::factory();
+		if ( $user->getId() && $lookup->isAttached( $user )
+			&& $lookup->isAttached( $user, $block->gb_by_wiki )
+		) {
+			$this->setBlocker( $user );
+			return;
+		}
+
+		// They don't exist locally, so we need to use an interwiki username
+		$this->setBlocker( "{$block->gb_by_wiki}>{$block->gb_by}" );
 	}
 }
