2a0f40154cc8492c9dd69038cc8b5b619a09d63c
Tks4Fish <tks4fish@gmail.com>
2022-01-21 18:24:27 -0300

    SpecialGlobalBlockList: add filtering options
    Bug: T178749
    Change-Id: I3d1b3fc10aa662c851d9e60abd762fadee7347c2

--- includes/specials/SpecialGlobalBlockList.php
+++ includes/specials/SpecialGlobalBlockList.php
@@ -1,15 +1,31 @@
 <?php
 
+use MediaWiki\Block\BlockUtils;
+use MediaWiki\Block\DatabaseBlock;
 use Wikimedia\IPUtils;
 
 class SpecialGlobalBlockList extends SpecialPage {
 	/** @var string|null */
 	protected $target;
 
-	public function __construct() {
+	/** @var array */
+	protected $options;
+
+	/** @var BlockUtils */
+	private $blockUtils;
+
+	public function __construct(
+		BlockUtils $blockUtils
+	) {
 		parent::__construct( 'GlobalBlockList' );
+
+		$this->blockUtils = $blockUtils;
 	}
 
+	/**
+	 * @param string $par Parameters of the URL, probably the IP being actioned
+	 * @return void
+	 */
 	public function execute( $par ) {
 		$this->setHeaders();
 		$this->outputHeader( 'globalblocking-list-intro' );
@@ -21,7 +37,7 @@ class SpecialGlobalBlockList extends SpecialPage {
 		$out->setArticleRelated( false );
 		$out->enableClientCache( false );
 
-		$this->loadParameters( $this->getRequest()->getText( 'ip', $par ) );
+		$this->loadParameters( $par );
 		$this->showForm();
 
 		// Validate search target. If it is invalid, no need to build the pager.
@@ -37,16 +53,20 @@ class SpecialGlobalBlockList extends SpecialPage {
 	}
 
 	/**
-	 * @param string $ip
+	 * @param string|null $par Parameter from the URL, may be null or a string (probably an IP)
+	 * that was inserted
 	 */
-	protected function loadParameters( $ip ) {
-		$ip = trim( $ip );
+	protected function loadParameters( $par ) {
+		$request = $this->getRequest();
+		$ip = trim( $request->getText( 'ip', $par ) );
 		if ( $ip !== '' ) {
 			$ip = IPUtils::isIPAddress( $ip )
 				? IPUtils::sanitizeRange( $ip )
 				: $ip;
 		}
 		$this->target = $ip;
+
+		$this->options = $request->getArray( 'wpOptions', [] );
 	}
 
 	protected function showForm() {
@@ -57,7 +77,17 @@ class SpecialGlobalBlockList extends SpecialPage {
 				'id' => 'mw-globalblocking-search-ip',
 				'label-message' => 'globalblocking-search-ip',
 				'default' => $this->target,
-			]
+			],
+			'Options' => [
+				'type' => 'multiselect',
+				'options-messages' => [
+					'globalblocking-list-tempblocks' => 'tempblocks',
+					'globalblocking-list-indefblocks' => 'indefblocks',
+					'globalblocking-list-addressblocks' => 'addressblocks',
+					'globalblocking-list-rangeblocks' => 'rangeblocks',
+				],
+				'flatlist' => true,
+			],
 		];
 		$context = new DerivativeContext( $this->getContext() );
 		$context->setTitle( $this->getPageTitle() ); // remove subpage
@@ -73,21 +103,45 @@ class SpecialGlobalBlockList extends SpecialPage {
 
 	protected function showList() {
 		$out = $this->getOutput();
+		$dbr = GlobalBlocking::getGlobalBlockingDatabase( DB_REPLICA );
 
 		// Build a list of blocks.
 		$conds = [];
-		$ip = $this->target;
 
-		if ( $ip ) {
-			list( $rangeStart, $rangeEnd ) = IPUtils::parseRange( $ip );
+		if ( $this->target !== '' ) {
+			[ $target, $type ] = $this->blockUtils->parseBlockTarget( $this->target );
 
-			if ( $rangeStart === $rangeEnd ) {
-				// They searched for an IP. Match any range covering that IP
-				$conds = GlobalBlocking::getRangeCondition( $ip );
-			} else {
-				// They searched for a range. Match that exact range only
-				$conds = [ 'gb_address' => $ip ];
+			switch ( $type ) {
+				case DatabaseBlock::TYPE_IP:
+					$conds = GlobalBlocking::getRangeCondition( $target );
+					break;
+				case DatabaseBlock::TYPE_RANGE:
+					$conds = [ 'gb_address' => $target ];
+					break;
 			}
+		}
+
+		$hideIP = in_array( 'addressblocks', $this->options );
+		$hideRange = in_array( 'rangeblocks', $this->options );
+
+		if ( $hideIP && $hideRange ) {
+			$this->noResults();
+			return;
+		} elseif ( $hideIP ) {
+			$conds[] = "gb_range_end > gb_range_start";
+		} elseif ( $hideRange ) {
+			$conds[] = "gb_range_end = gb_range_start";
+		}
+
+		$hideTemp = in_array( 'tempblocks', $this->options );
+		$hideIndef = in_array( 'indefblocks', $this->options );
+		if ( $hideTemp && $hideIndef ) {
+			$this->noResults();
+			return;
+		} elseif ( $hideTemp ) {
+			$conds[] = "gb_expiry = " . $dbr->addQuotes( $dbr->getInfinity() );
+		} elseif ( $hideIndef ) {
+			$conds[] = "gb_expiry != " . $dbr->addQuotes( $dbr->getInfinity() );
 		}
 
 		$pager = new GlobalBlockListPager( $this->getContext(), $conds, $this->getLinkRenderer() );
@@ -99,11 +153,19 @@ class SpecialGlobalBlockList extends SpecialPage {
 				$pager->getNavigationBar()
 			);
 		} else {
-			$out->wrapWikiMsg(
-				"<div class='mw-globalblocking-noresults'>\n$1</div>\n",
-				[ 'globalblocking-list-noresults' ]
-			);
+			$this->noResults();
 		}
+	}
+
+	/**
+	 * Display an error when no results are found for those parameters
+	 * @return void
+	 */
+	private function noResults() {
+		$this->getOutput()->wrapWikiMsg(
+			"<div class='mw-globalblocking-noresults'>\n$1</div>\n",
+			[ 'globalblocking-list-noresults' ]
+		);
 	}
 
 	protected function getGroupName() {
