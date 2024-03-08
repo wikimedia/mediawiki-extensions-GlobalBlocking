2421bf90bc37695d470a928e3e285e295020f1a2
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-03-08 17:10:40 +0000

    Partially update SpecialGlobalBlock for global account blocks
    Why:
    * To be able to test I0c6a26c627f7e62d25d1d427ee68d6eeb2e6baff,
      the Special:GlobalBlock page needs to be updated to support
      global blocks on accounts.
    * These changes are limited to what is needed to effectively test
      the patch that this is based on-top of. Other changes will be
      made in a future commit.
    
    What:
    * Update SpecialGlobalBlock::setParameter to accept and normalise
      username targets, and update ::loadExistingBlock to be able to
      find blocks on accounts.
    * Properly escape the username on the success page, as it is
      currently treated as wikitext (which could cause problems for
      users).
    * Add integration tests to ensure consistent test coverage over
      the modified code.
    
    Bug: T358157
    Bug: T356934
    Change-Id: Idbbe8f882c816b85c1086cda182b4fadfb1136a3

--- includes/Special/SpecialGlobalBlock.php
+++ includes/Special/SpecialGlobalBlock.php
@@ -8,11 +8,15 @@ use MediaWiki\Block\BlockUserFactory;
 use MediaWiki\Block\BlockUtils;
 use MediaWiki\CommentStore\CommentStore;
 use MediaWiki\Extension\GlobalBlocking\GlobalBlocking;
+use MediaWiki\Extension\GlobalBlocking\Services\GlobalBlockingConnectionProvider;
+use MediaWiki\Extension\GlobalBlocking\Services\GlobalBlockManager;
 use MediaWiki\Html\Html;
 use MediaWiki\SpecialPage\FormSpecialPage;
 use MediaWiki\Status\Status;
 use MediaWiki\Title\Title;
+use MediaWiki\User\CentralId\CentralIdLookup;
 use MediaWiki\User\UserIdentity;
+use MediaWiki\User\UserNameUtils;
 use Wikimedia\IPUtils;
 
 class SpecialGlobalBlock extends FormSpecialPage {
@@ -20,31 +24,43 @@ class SpecialGlobalBlock extends FormSpecialPage {
 	 * @see SpecialGlobalBlock::setParameter()
 	 * @var string|null
 	 */
-	protected $address;
+	protected ?string $target;
 
 	/**
-	 * Whether there is an existing block on the target
-	 * @var bool
+	 * @var bool Whether there is an existing block on the target
 	 */
-	private $modifyForm = false;
+	private bool $modifyForm = false;
 
-	/** @var BlockUserFactory */
-	private $blockUserFactory;
-
-	/** @var BlockUtils */
-	private $blockUtils;
+	private BlockUserFactory $blockUserFactory;
+	private BlockUtils $blockUtils;
+	private GlobalBlockingConnectionProvider $globalBlockingConnectionProvider;
+	private GlobalBlockManager $globalBlockManager;
+	private CentralIdLookup $centralIdLookup;
+	private UserNameUtils $userNameUtils;
 
 	/**
 	 * @param BlockUserFactory $blockUserFactory
 	 * @param BlockUtils $blockUtils
+	 * @param GlobalBlockingConnectionProvider $globalBlockingConnectionProvider
+	 * @param GlobalBlockManager $globalBlockManager
+	 * @param CentralIdLookup $centralIdLookup
+	 * @param UserNameUtils $userNameUtils
 	 */
 	public function __construct(
 		BlockUserFactory $blockUserFactory,
-		BlockUtils $blockUtils
+		BlockUtils $blockUtils,
+		GlobalBlockingConnectionProvider $globalBlockingConnectionProvider,
+		GlobalBlockManager $globalBlockManager,
+		CentralIdLookup $centralIdLookup,
+		UserNameUtils $userNameUtils
 	) {
 		parent::__construct( 'GlobalBlock', 'globalblock' );
 		$this->blockUserFactory = $blockUserFactory;
 		$this->blockUtils = $blockUtils;
+		$this->globalBlockingConnectionProvider = $globalBlockingConnectionProvider;
+		$this->globalBlockManager = $globalBlockManager;
+		$this->centralIdLookup = $centralIdLookup;
+		$this->userNameUtils = $userNameUtils;
 	}
 
 	public function doesWrites() {
@@ -65,23 +81,30 @@ class SpecialGlobalBlock extends FormSpecialPage {
 	 */
 	protected function setParameter( $par ) {
 		if ( $par && !$this->getRequest()->wasPosted() ) {
-			// GET request to Special:GlobalBlock/127.0.0.1
-			$address = $par;
+			// GET request to Special:GlobalBlock/target where 'target' can be an IP, range, or username.
+			$target = $par;
 		} else {
-			$address = trim( $this->getRequest()->getText( 'wpAddress' ) );
+			$target = trim( $this->getRequest()->getText( 'wpAddress' ) );
 		}
 
-		if ( IPUtils::isValidRange( $address ) ) {
-			$this->address = IPUtils::sanitizeRange( $address );
+		if ( IPUtils::isValidRange( $target ) ) {
+			$this->target = IPUtils::sanitizeRange( $target );
+		} elseif ( IPUtils::isIPAddress( $target ) ) {
+			$this->target = IPUtils::sanitizeIP( $target );
 		} else {
-			// This catches invalid IPs too but we'll reject them at form submission.
-			$this->address = IPUtils::sanitizeIP( $address );
+			$normalisedTarget = $this->userNameUtils->getCanonical( $target );
+			if ( $normalisedTarget ) {
+				$this->target = $normalisedTarget;
+			} else {
+				// Allow invalid targets to be set, so that the user can be shown an error message.
+				$this->target = $target;
+			}
 		}
 
-		[ $target ] = $this->blockUtils->parseBlockTarget( $this->address );
+		[ $targetForSkin ] = $this->blockUtils->parseBlockTarget( $target );
 
-		if ( $target instanceof UserIdentity ) {
-			$this->getSkin()->setRelevantUser( $target );
+		if ( $targetForSkin instanceof UserIdentity ) {
+			$this->getSkin()->setRelevantUser( $targetForSkin );
 		}
 	}
 
@@ -92,18 +115,26 @@ class SpecialGlobalBlock extends FormSpecialPage {
 	 *
 	 * @return array
 	 */
-	protected function loadExistingBlock() {
+	protected function loadExistingBlock(): array {
 		$blockOptions = [];
-		if ( $this->address ) {
-			$dbr = GlobalBlocking::getReplicaGlobalBlockingDatabase();
-			$block = $dbr->selectRow( 'globalblocks',
-					[ 'gb_anon_only', 'gb_reason', 'gb_expiry' ],
-					[
-						'gb_address' => $this->address,
-						'gb_expiry >' . $dbr->addQuotes( $dbr->timestamp( wfTimestampNow() ) ),
-					],
-					__METHOD__
-				);
+		if ( $this->target ) {
+			$dbr = $this->globalBlockingConnectionProvider->getReplicaGlobalBlockingDatabase();
+			$queryBuilder = $dbr->newSelectQueryBuilder()
+				->select( [ 'gb_anon_only', 'gb_reason', 'gb_expiry' ] )
+				->from( 'globalblocks' );
+			if ( IPUtils::isIPAddress( $this->target ) ) {
+				$queryBuilder->where( [ 'gb_address' => $this->target ] );
+			} else {
+				$centralId = $this->centralIdLookup->centralIdFromName( $this->target );
+				if ( !$centralId ) {
+					return [];
+				}
+				$queryBuilder->where( [ 'gb_target_central_id' => $centralId ] );
+			}
+			$block = $queryBuilder
+				->andWhere( [ $dbr->expr( 'gb_expiry', '>', $dbr->timestamp() ) ] )
+				->caller( __METHOD__ )
+				->fetchRow();
 			if ( $block ) {
 				$this->modifyForm = true;
 
@@ -130,7 +161,7 @@ class SpecialGlobalBlock extends FormSpecialPage {
 				'id' => 'mw-globalblock-address',
 				'required' => true,
 				'autofocus' => true,
-				'default' => $this->address,
+				'default' => $this->target,
 			],
 			'Expiry' => [
 				'type' => count( $getExpiry ) ? 'selectorother' : 'text',
@@ -158,7 +189,7 @@ class SpecialGlobalBlock extends FormSpecialPage {
 			],
 			'Previous' => [
 				'type' => 'hidden',
-				'default' => $this->address,
+				'default' => $this->target,
 			],
 		];
 
@@ -168,7 +199,7 @@ class SpecialGlobalBlock extends FormSpecialPage {
 			$fields['Expiry']['default'] = $blockOptions['expiry'];
 			$fields['Reason']['default'] = $blockOptions['reason'];
 			$fields['AnonOnly']['default'] = $blockOptions['anononly'];
-			if ( $this->getRequest()->getVal( 'Previous' ) !== $this->address ) {
+			if ( $this->getRequest()->getVal( 'Previous' ) !== $this->target ) {
 				// Let the user know about it and re-submit to modify
 				$fields['Modify']['default'] = 1;
 			}
@@ -214,7 +245,7 @@ class SpecialGlobalBlock extends FormSpecialPage {
 		if ( $this->modifyForm && !$this->getRequest()->wasPosted() ) {
 			// For GET requests with target field prefilled, tell the user that it's already blocked
 			// (For POST requests, this will be shown to the user as an actual error in HTMLForm)
-			$msg = $this->msg( 'globalblocking-block-alreadyblocked', $this->address )->parseAsBlock();
+			$msg = $this->msg( 'globalblocking-block-alreadyblocked', $this->target )->parseAsBlock();
 			$form->addHeaderHtml( Html::rawElement( 'div', [ 'class' => 'error' ], $msg ) );
 		}
 
@@ -230,7 +261,7 @@ class SpecialGlobalBlock extends FormSpecialPage {
 	 */
 	protected function postHtml() {
 		$out = '';
-		$title = Title::makeTitleSafe( NS_USER, $this->address );
+		$title = Title::makeTitleSafe( NS_USER, $this->target );
 		if ( $title ) {
 			LogEventsList::showLogExtract(
 				$out,
@@ -265,8 +296,8 @@ class SpecialGlobalBlock extends FormSpecialPage {
 		}
 
 		// This handles validation too...
-		$globalBlockStatus = GlobalBlocking::block(
-			$this->address, // $this->address is sanitized; $data['Address'] isn't
+		$globalBlockStatus = $this->globalBlockManager->block(
+			$this->target, // $this->target is sanitized; $data['Address'] isn't
 			$data['Reason'][0],
 			$data['Expiry'],
 			$performer,
@@ -281,7 +312,7 @@ class SpecialGlobalBlock extends FormSpecialPage {
 		// Add a local block if the user asked for that
 		if ( $performer->isAllowed( 'block' ) && $data['AlsoLocal'] ) {
 			$localBlockStatus = $this->blockUserFactory->newBlockUser(
-				$this->address,
+				$this->target,
 				$performer,
 				$data['Expiry'],
 				$data['Reason'][0],
@@ -305,7 +336,10 @@ class SpecialGlobalBlock extends FormSpecialPage {
 	public function onSuccess() {
 		$successMsg = $this->modifyForm ?
 			'globalblocking-modify-success' : 'globalblocking-block-success';
-		$this->getOutput()->addWikiMsg( $successMsg, $this->address );
+		// The username must be escaped here, as it's user input and could contain wikitext.
+		$this->getOutput()->addHTML(
+			$this->msg( $successMsg )->plaintextParams( $this->target )->parseAsBlock()
+		);
 
 		$link = $this->getLinkRenderer()->makeKnownLink(
 			$this->getPageTitle(),
---
+++ tests/phpunit/Integration/Special/SpecialGlobalBlockTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace MediaWiki\Extension\GlobalBlocking\Test\Integration\Special;
+
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Extension\GlobalBlocking\GlobalBlockingServices;
+use MediaWiki\Request\FauxRequest;
+use MediaWiki\Tests\SpecialPage\FormSpecialPageTestCase;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @covers \MediaWiki\Extension\GlobalBlocking\Special\SpecialGlobalBlock
+ * @group Database
+ */
+class SpecialGlobalBlockTest extends FormSpecialPageTestCase {
+
+	protected function setUp(): void {
+		parent::setUp();
+		// We don't want to test specifically the CentralAuth implementation of the CentralIdLookup. As such, force it
+		// to be the local provider.
+		$this->setMwGlobals( 'wgCentralIdLookupProvider', 'local' );
+	}
+
+	protected function newSpecialPage() {
+		return $this->getServiceContainer()->getSpecialPageFactory()->getPage( 'GlobalBlock' );
+	}
+
+	/** @dataProvider provideSetParameter */
+	public function testSetParameter( $providedTarget, $fromSubpage, $expectedTarget ) {
+		if ( !$fromSubpage ) {
+			$mockRequest = new FauxRequest( [ 'wpAddress' => $providedTarget ], true );
+			RequestContext::getMain()->setRequest( $mockRequest );
+		}
+		$specialGlobalBlock = TestingAccessWrapper::newFromObject( $this->newSpecialPage() );
+		$specialGlobalBlock->setParameter( $fromSubpage ? $providedTarget : '' );
+		$this->assertSame( $expectedTarget, $specialGlobalBlock->target );
+	}
+
+	public static function provideSetParameter() {
+		return [
+			'Empty target from request' => [ '', false, '' ],
+			'Empty target from subpage' => [ '', true, '' ],
+			'IP target from request' => [ '127.0.0.1', false, '127.0.0.1' ],
+			'IP target from subpage' => [ '127.0.0.1', true, '127.0.0.1' ],
+			'IP range from subpage' => [ '1.2.3.4/24', true, '1.2.3.0/24' ],
+			'User from request' => [ 'testing_test', false, 'Testing test' ],
+		];
+	}
+
+	public function testLoadExistingBlockWithExistingBlock() {
+		// Perform a block on 127.0.0.1 so that we can test the loadExistingBlock method returning
+		// data on an existing block.
+		$testTarget = $this->getTestUser()->getUser();
+		GlobalBlockingServices::wrap( $this->getServiceContainer() )->getGlobalBlockManager()->block(
+			$testTarget->getName(),
+			'test',
+			'infinite',
+			$this->getTestUser( [ 'steward' ] )->getUser()
+		);
+		// Set the target to the user which was blocked.
+		$specialGlobalBlock = TestingAccessWrapper::newFromObject( $this->newSpecialPage() );
+		$specialGlobalBlock->setParameter( $testTarget->getName() );
+		// Call the loadExistingBlock method
+		$this->assertArrayEquals(
+			[
+				'anononly' => 0,
+				'reason' => 'test',
+				'expiry' => 'indefinite',
+			],
+			$specialGlobalBlock->loadExistingBlock(),
+			false,
+			true
+		);
+	}
+
+	/** @dataProvider provideTargetsWhichAreNotBlocked */
+	public function testLoadExistingBlockWithNoBlock( $username ) {
+		// Set the target to 127.0.0.1, which is not blocked.
+		$specialGlobalBlock = TestingAccessWrapper::newFromObject( $this->newSpecialPage() );
+		$specialGlobalBlock->setParameter( $username );
+		// Call the loadExistingBlock method
+		$this->assertArrayEquals( [], $specialGlobalBlock->loadExistingBlock() );
+	}
+
+	public static function provideTargetsWhichAreNotBlocked() {
+		return [
+			'IP address' => [ '127.0.0.1' ],
+			'Non-existent user' => [ 'Non-existent test user1234' ],
+		];
+	}
+}
