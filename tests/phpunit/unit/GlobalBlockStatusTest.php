515209679599af02cccec47dd924c4553c4903ac
Umherirrender <umherirrender_de.wp@web.de>
2026-02-27 23:46:43 +0100

    tests: Use Status-specific assertion functions
    If the assertions fail, these functions provide more detail than
    normal assertion functions like assertTrue
    
    Change-Id: I6fdc91590cf77f1fac504eb722f32bf456d0a397

--- tests/phpunit/unit/GlobalBlockStatusTest.php
+++ tests/phpunit/unit/GlobalBlockStatusTest.php
@@ -13,44 +13,44 @@ class GlobalBlockStatusTest extends MediaWikiUnitTestCase {
 	public function testNewGood() {
 		$status = GlobalBlockStatus::newGood( 'value' );
 		$this->assertInstanceOf( GlobalBlockStatus::class, $status );
-		$this->assertSame( 'value', $status->getValue() );
+		$this->assertStatusValue( 'value', $status );
 		$this->assertTrue( $status->isGlobalBlockOK() );
 		$this->assertFalse( $status->isLocalBlockOK() );
 		$this->assertFalse( $status->hasLocalBlockError() );
-		$this->assertTrue( $status->getGlobalStatus()->isOK() );
+		$this->assertStatusOK( $status->getGlobalStatus() );
 	}
 
 	public function testNewFatal() {
 		$status = GlobalBlockStatus::newFatal( 'some-message' );
 		$this->assertInstanceOf( GlobalBlockStatus::class, $status );
-		$this->assertSame( 'some-message', $status->getMessages()[0]->getKey() );
-		$this->assertFalse( $status->isOK() );
+		$this->assertStatusMessage( 'some-message', $status );
+		$this->assertStatusNotOK( $status );
 		$this->assertFalse( $status->isGlobalBlockOK() );
 		$this->assertFalse( $status->isLocalBlockOK() );
 		$this->assertFalse( $status->hasLocalBlockError() );
-		$this->assertFalse( $status->getGlobalStatus()->isOK() );
+		$this->assertStatusNotOK( $status->getGlobalStatus() );
 	}
 
 	public function testWithLocalStatusFatal() {
 		$status = GlobalBlockStatus::newGood( 'value' );
 		$status = $status->withLocalStatus( StatusValue::newFatal( 'some-message' ) );
-		$this->assertFalse( $status->isOK() );
+		$this->assertStatusNotOK( $status );
 		$this->assertTrue( $status->isGlobalBlockOK() );
 		$this->assertFalse( $status->isLocalBlockOK() );
 		$this->assertTrue( $status->hasLocalBlockError() );
-		$this->assertSame( 'some-message', $status->getMessages()[0]->getKey() );
-		$this->assertSame( 'value', $status->getValue() );
-		$this->assertTrue( $status->getGlobalStatus()->isOK() );
+		$this->assertStatusMessage( 'some-message', $status );
+		$this->assertStatusValue( 'value', $status );
+		$this->assertStatusOK( $status->getGlobalStatus() );
 	}
 
 	public function testWithLocalStatusGood() {
 		$status = GlobalBlockStatus::newGood( 'value' );
 		$status = $status->withLocalStatus( StatusValue::newGood( 'local-value' ) );
-		$this->assertTrue( $status->isOK() );
+		$this->assertStatusOK( $status );
 		$this->assertTrue( $status->isGlobalBlockOK() );
 		$this->assertTrue( $status->isLocalBlockOK() );
 		$this->assertFalse( $status->hasLocalBlockError() );
-		$this->assertSame( 'value', $status->getValue() );
-		$this->assertTrue( $status->getGlobalStatus()->isOK() );
+		$this->assertStatusValue( 'value', $status );
+		$this->assertStatusOK( $status->getGlobalStatus() );
 	}
 }
