a59f2187da44a3df3e128b8f9bf918dee243e1f5
Dreamy Jazz <wpgbrown@wikimedia.org>
2026-03-04 19:40:00 +0000

    Drop GlobalBlockLocalStatusLookup::getLocalWhitelistInfo
    Why:
    * GlobalBlockLocalStatusLookup::getLocalWhitelistInfo has been
      hard deprecated since 8cddff043063cec1eea8ff228ccedffdecef9707
      which was merged a year ago
    * This means that it is safe to drop the method as it has
      been long enough after hard deprecation to drop it
    ** Furthermore, no code calls this method so dropping it will
       not break anything (per a search in codesearch)
    * We are modifying the file shortly to add a new method,
      so should cleanup this service first
    
    What:
    * Drop GlobalBlockLocalStatusLookup::getLocalWhitelistInfo
    
    Bug: T277942
    Change-Id: I54d806b5a6ae439bebe0282990c702e6b8ce85c2

--- includes/Services/GlobalBlockLocalStatusLookup.php
+++ includes/Services/GlobalBlockLocalStatusLookup.php
@@ -2,10 +2,8 @@
 
 namespace MediaWiki\Extension\GlobalBlocking\Services;
 
-use InvalidArgumentException;
 use MediaWiki\Block\AutoblockExemptionList;
 use MediaWiki\User\CentralId\CentralIdLookup;
-use Wikimedia\IPUtils;
 use Wikimedia\Rdbms\IConnectionProvider;
 
 /**
@@ -89,56 +87,5 @@ class GlobalBlockLocalStatusLookup {
 			// Block has been locally disabled.
 			return [ 'user' => (int)$row->gbw_by, 'reason' => $row->gbw_reason ];
 		}
-	}
-
-	/**
-	 * Returns whether the given global block ID or block on a specific target has been
-	 * locally disabled.
-	 *
-	 * @param int|null $id Block ID
-	 * @param null|string $target The target of the block (only used if $id is null). Can be an IP, range, or username.
-	 *    You should use the $id over the $target parameter if you have the $id, as using the $id is quicker.
-	 * @param string|false $wikiId The wiki where the where the whitelist info should be looked up.
-	 *   Use false for the local wiki.
-	 * @return array|false false if the block is not locally disabled, otherwise an array containing the
-	 *   user ID of the user who disabled the block and the reason for the block being disabled.
-	 * @deprecated Since 1.43. Use {@link GlobalBlockLocalStatusLookup::getLocalStatusInfo}, which no longer accepts
-	 *   a target parameter but returns the same data when passed a global block ID. To get the global block ID use the
-	 *   {@link GlobalBlockLookup::getGlobalBlockId} method.
-	 * @phan-return array{user:int,reason:string}|false
-	 */
-	public function getLocalWhitelistInfo( ?int $id = null, ?string $target = null, $wikiId = false ) {
-		wfDeprecated( __METHOD__, '1.43' );
-		if ( $target === null && $id === null ) {
-			throw new InvalidArgumentException(
-				'Both $target and $id are null when attempting to retrieve whitelist status'
-			);
-		} elseif ( $target !== null ) {
-			$globalBlockIdQueryBuilder = $this->globalBlockingConnectionProvider
-				->getReplicaGlobalBlockingDatabase()
-				->newSelectQueryBuilder()
-				->select( 'gb_id' )
-				->from( 'globalblocks' );
-			if ( IPUtils::isIPAddress( $target ) ) {
-				$globalBlockIdQueryBuilder->where( [ 'gb_address' => $target ] );
-			} else {
-				$centralIdForTarget = $this->centralIdLookup->centralIdFromName(
-					$target, CentralIdLookup::AUDIENCE_RAW
-				);
-				if ( !$centralIdForTarget ) {
-					// If the target does not have a central ID, we cannot look up by username. In this case we can
-					// assume that there is no global block and therefore it cannot have a local status.
-					return false;
-				}
-				$globalBlockIdQueryBuilder->where( [ 'gb_target_central_id' => $centralIdForTarget ] );
-			}
-			$id = $globalBlockIdQueryBuilder->fetchField();
-			if ( !$id ) {
-				// Not locally disabled if there is no global block on the target
-				return false;
-			}
-		}
-
-		return $this->getLocalStatusInfo( $id, $wikiId );
 	}
 }
